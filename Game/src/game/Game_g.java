c69405ae69064cf191fed7ca0d7ab63a2801c12a
Arlax1011 <31315854+Arlax1011@users.noreply.github.com>
2019-11-26 12:56:12 +0100

    Add files via upload

--- Game/src/game/Game_g.java
+++ Game/src/game/Game_g.java
@@ -1,30 +1,71 @@
 
 package game;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 public class Game_g {
     
     private Random dice = new Random();
-    private Player player;
+    //private Player player;
     
-    public void addPlayer(Player p){
+    /*public void addPlayer(Player p){
         this.player=p;
-    }
+    }*/
+    
+    private List<Player> players = new ArrayList();
+   
+    public void addPlayer(Player player){
+        //this.player = player;
+        
+        boolean modify=false;
+        for(Player p: players){
+            if(p.getName().equals(player.getName())){
+                modify=true;
+            }
+        }
+        if(!modify) players.add(player);
+        else{
+            player.setName(player.getName() + dice.nextInt(2));
             
+            this.addPlayer(player);
+        }
+    }
+    
     public void play(){
             
         int pc,guess;
         
-        System.out.println(player.getName() + " zgaduje wylosowana liczbe");
+        boolean repeat;
+        
         do{
-        
+            
+            repeat=true;
+            
             pc = dice.nextInt(6)+1;
-            guess = player.guess();
-            System.out.println("Wylosowana liczba: "+pc + " Liczba gracza: " + guess);
-        
-        }while(pc!=guess);
-        System.out.println("Gratulacje!");
+            
+            System.out.println("########################");
+            
+            System.out.println("Wylosowana liczba: "+pc);
+            
+            
+            for(Iterator<Player> it = players.iterator(); it.hasNext();){
+                
+                Player player = it.next();
+                guess = player.guess();
+                System.out.println("Liczba gracza "+player.getName()+": " + guess);
+                if(pc!=guess){
+                    System.out.println("Zle");
+                }
+                else{
+                    System.out.println("Dobrze!");
+                    repeat=false;
+                }
+            }
+            
+        }while(repeat);
         
     }
     
